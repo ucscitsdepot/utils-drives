f43dcb3f5c083a74b7acca030807daa4528e39e1
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2024-11-07 17:19:58 -0800

    wipe full drive even if no partitions

--- litetouch.sh
+++ litetouch.sh
@@ -19,7 +19,7 @@ if [ -n $ISO ]; then
 
     echo "Found LiteTouch at ${ISO}"
 
-    wipefs -a ${TGTDEV}1
+    wipefs -a ${TGTDEV}
     echo "Wiped ${TGTDEV}"
     sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' <<EOF | fdisk ${TGTDEV}
 g # clear the in memory partition table
