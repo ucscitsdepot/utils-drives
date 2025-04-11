1e1165b0137d69ca1fdfa9d2574cc20ee901abb0
Ishan Madan <19366470+ishanm0@users.noreply.github.com>
2025-04-11 09:23:56 -0700

    comment shell scripts

--- 4-in-1.sh
+++ 4-in-1.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# check if the script is run as root (necessary for dd)
 if [ "$(id -u)" -ne 0 ]; then
     echo "Please run as root." >&2
     exit 1
 fi
 
+# script must be run w/ 1 argument (the target device)
 if [ $# -lt 1 ]; then
     echo "Usage: sudo ./4-in-1.sh /dev/sdX" >&2
     exit 1
@@ -12,4 +14,7 @@ fi
 
 TGTDEV=$1
 
+# TODO: verify that the target device exists, is a usb drive with a /dev/sdX path, is writable, etc
+
+# dd command to write the image to the target device
 dd if=/home/depot/utils/4-in-1-mac.img of=$TGTDEV bs=4M status=progress
--- litetouch.sh
+++ litetouch.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
+# if an error occurs, exit immediately
 set -e
 
+# check if the script is run as root (necessary for disk operations)
 if [ "$(id -u)" -ne 0 ]; then
     echo "Please run as root." >&2
     exit 1
 fi
 
+# script must be run w/ 1 argument (the target device)
 if [ $# -lt 1 ]; then
     echo "Usage: sudo ./litetouch.sh /dev/sdX" >&2
     exit 1
 fi
 
+# check if another LiteTouch installation is in progress (is the /mnt/tmp directory empty?)
+if [ ! -z "$(ls -A '/mnt/tmp')" ]; then
+    echo "Another LiteTouch installation is in progress. Please wait until it is finished. If you're sure there isn't another LiteTouch install in progress, unmount the device at /mnt/tmp" >&2
+    exit 1
+fi
+
+# get target device
 TGTDEV=$1
+
+# find the latest LiteTouch ISO
 ISO="$(find ~+ -type f -name "SDSLiteTouch*" -print0 | xargs -r -0 ls -1 -t | head -1)"
 
+# if ISO is found
 if [ -n $ISO ]; then
-
     echo "Found LiteTouch at ${ISO}"
 
+    # wipe the target device
     wipefs -a ${TGTDEV}
     echo "Wiped ${TGTDEV}"
+
+    # pass input to fdisk to create a new partition
     sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' <<EOF | fdisk ${TGTDEV}
 g # clear the in memory partition table
 n # new partition
@@ -35,28 +50,35 @@ EOF
 
     echo "Formatted ${TGTDEV}"
 
+    # create & check a FAT32 filesystem
     mkfs.vfat -n DVD_ROM ${TGTDEV}1
     fsck.vfat ${TGTDEV}1
 
     echo "Created filesystem"
 
+    # create mount points (if they don't exist)
     mkdir -p /mnt/tmp
     mkdir -p /mnt/iso
 
+    # mount the target device and the ISO
     mount ${TGTDEV}1 /mnt/tmp
     mount -o loop,ro $ISO /mnt/iso/
 
     echo "Begin copying files"
 
+    # copy the files from the ISO to the target device
     cp -r /mnt/iso/* /mnt/tmp
 
     echo "Finished copying files"
     echo "Unmounting..."
 
+    # unmount both filesystems
     umount /mnt/tmp
     umount /mnt/iso
 
     echo "Done"
 else
+    # if no ISO is found
     echo "No LiteTouch ISO found"
+    exit 1
 fi
